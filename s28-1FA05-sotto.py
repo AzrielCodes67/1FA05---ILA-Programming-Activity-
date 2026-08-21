63ffd599699e0c559660206fe3a7c759b47c8f0c
AzrielCodes67 <aedsotto@cmc.pshs.edu.ph>
2026-08-21 21:39:57 +0800

    Delete s28-1FA05-sotto.py

--- s28-1FA05-sotto.py
+++
@@ -1,8 +0,0 @@
-score = float(input("Enter the student's score"))
-
-# I used float instead of int because it is more safer to choose without knowing if there could be decimals.
-
-if score >= 90:
-    print ("Excellent!")
-else:
-    print ("Keep Practicing.")
